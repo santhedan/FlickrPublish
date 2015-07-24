8460fac7bc910b6ec0c62d61344720d0cb5aa222
Sanjay Dandekar <sanjay.dandekar@gmail.com>
2015-07-24 16:55:51 +0530

    Getting comments, public status and gave count
    Getting comments, public status and gave count

---
+++ FlickrPublish/PhotosGetFavorites.h
@@ -0,0 +1,27 @@
+//
+//  PhotosGetFavorites.h
+//  FlickrPublish
+//
+//  Created by Sanjay Dandekar on 24/07/15.
+//  Copyright (c) 2015 Sanjay Dandekar. All rights reserved.
+//
+
+#import "BaseRequest.h"
+
+@interface PhotosGetFavorites : BaseRequest
+
+@property (nonatomic, strong) NSString* photoId;
+
+@property (nonatomic, strong) NSString* authToken;
+
+@property (nonatomic, strong) NSString* method;
+
+@property (nonatomic, strong) NSString* nojsoncallback;
+
+@property (nonatomic, strong) NSString* format;
+
+- (instancetype) initWithKey: (NSString *) key Secret: (NSString *) secret Token: (NSString *) token PhotoId: (NSString *) photoId;
+
+- (NSString *) getUrl;
+
+@end
